901a3551ea28323ce3d8658e80e3ea7b74cc8aac
Ivan Hirsinger <ivan.hirsinger@gmail.com>
2022-03-29 08:21:55 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Ivan Hirsinger on 29.03.2022..
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

2431e4fe2ea22b0a92937b71e9e27105b5a7ccf7
Ivan Hirsinger <ivan.hirsinger@gmail.com>
2022-03-29 12:49:46 +0200

    -

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Ivan Hirsinger on 29.03.2022..
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+}
--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,14 +9,30 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    // List of modules
     @Published var modules = [Module]()
     
+    // Current module
+    @Published var currentModule: Module?
+    var currentModuleIndex = 0
+    
+    // Current lesson
+    @Published var currentLesson: Lesson?
+    var currentLessonIndex = 0
+    
+    // Current lesson explanation
+    @Published var lessonDescription = NSAttributedString()
     var styleData: Data?
+    
+    // Current selected content and test
+    @Published var currentContentSelected:Int?
     
     init() {
         
         getLocalData()
     }
+    
+    // MARK: Data methods
     
     func getLocalData() {
         // Get a url to the json file
@@ -49,5 +65,85 @@ class ContentModel: ObservableObject {
         catch {
             print("Couldn't parse style data.")
         }
+    }
+    
+    // MARK: Module navigation methods
+    
+    func beginModule(_ moduleid: Int) {
+        
+        // Find the index for this module id
+        for index in 0..<modules.count {
+            if modules[index].id == moduleid {
+                
+                // Found the matching module
+                currentModuleIndex = index
+                break
+            }
+        }
+        
+        // Set the current module
+        currentModule = modules[currentModuleIndex]
+    }
+    
+    func beginLesson(_ lessonIndex:Int) {
+        
+        // Check that the lesson index is within range of module lessons
+        if lessonIndex < currentModule!.content.lessons.count {
+            currentLessonIndex = lessonIndex
+        }
+        else {
+            currentLessonIndex = 0
+        }
+        
+        // Set the current lesson
+        currentLesson = currentModule!.content.lessons[currentLessonIndex]
+        lessonDescription = addStyling(currentLesson!.explanation)
+    }
+    
+    func nextLesson() {
+        
+        // Advance the lesson index
+        currentLessonIndex += 1
+        
+        // Check that it is within the range
+        if currentLessonIndex < currentModule!.content.lessons.count {
+            
+            // Set the current lesson property
+            currentLesson = currentModule!.content.lessons[currentLessonIndex]
+            lessonDescription = addStyling(currentLesson!.explanation)
+        }
+        else {
+            // Reset the lesson state
+            currentLesson = nil
+            currentLessonIndex = 0
+        }
+    }
+    
+    func hasNextLesson() -> Bool {
+      
+        return (currentLessonIndex + 1 < currentModule!.content.lessons.count)
+    }
+    
+    // MARK: Code Styling
+    
+    private func addStyling(_ htmlString: String) -> NSAttributedString {
+        
+        var resultString = NSAttributedString()
+        var data = Data()
+        
+        // Add the styling data
+        if styleData != nil {
+            data.append(styleData!)
+        }
+        
+        // Add the html data
+        data.append(Data(htmlString.utf8))
+        
+        // Convert to attributed string
+        if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+            
+            resultString = attributedString
+        }
+        return resultString
     }
 }
